1e19471a367d77d24701bffe57db272a470cda2a
Sara Jahedazad <114187084+sarajahedazad@users.noreply.github.com>
2025-02-09 14:27:41 -0500

    Update test_newton_solver.py

--- tests/test_newton_solver.py
+++ tests/test_newton_solver.py
@@ -14,3 +14,15 @@ def test_newtonsolver():
     found = ns.solver( F, J, x, x0, verbose = True)
     known = 2
     assert np.isclose( known , found )
+
+    n = 2  # Number of variables
+    x = sympy.symbols(f'x:{n}')  # Creates [x0, x1] dynamically
+    
+    F = sympy.Matrix([x[0]**3- 4 *x[0], x[1]**2- 4 *x[1]])  # Example function
+    J = F.jacobian(x)
+    x0 = np.array([1.5, 2.2])
+    
+    found = ns.solver( F, J, x, x0, verbose = True)
+    known = np.array([2, 4])
+    assert np.isclose( known , found )
+
