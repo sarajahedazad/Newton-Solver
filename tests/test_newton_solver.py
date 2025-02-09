64be76536bf8eb147266d2209b10d3b23ad6fbce
Sara Jahedazad <114187084+sarajahedazad@users.noreply.github.com>
2025-02-09 00:41:16 -0500

    Update test_newton_solver.py

--- tests/test_newton_solver.py
+++ tests/test_newton_solver.py
@@ -10,9 +10,7 @@ def test_newtonsolver():
     F = sympy.Matrix([ x[0]**3- 4 *x[0] ])
     J = F.jacobian(x)
     x0 = np.array([ 6 ])
-    root = ns.solver( F, J, x, x0, verbose = True)
 
-    print( root )
-    known = 2
     found = ns.solver( F, J, x, x0, verbose = True)
+    known = 2
     assert np.isclose( known , found )
